5ef12ee298502fa31d1071d2dcfd9a18af4d0e4c
coderpwh <pengwenhao@aliyun.com>
2017-12-28 09:46:17 +0800

    update java8 code

---
+++ java/jdk8/source/jdk8/src/main/java/com/coderpwh/stream/Streams10.java
@@ -0,0 +1,119 @@
+package com.coderpwh.stream;
+
+import com.beust.jcommander.internal.Lists;
+
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+/**
+ * Created by coderpwh on 2017/12/28.
+ */
+public class Streams10 {
+
+    static class Person{
+
+        String name;
+        int age;
+
+        Person(String name,int age){
+            this.name = name;
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+             return name;
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+
+        List<Person> persons = Lists.newArrayList(new Person("Max",18),
+                new Person("Peter",23),
+                new Person("Pamela",23),
+                new Person("David",12));
+
+//             test1(persons);
+//             test2(persons);
+//               test3(persons);
+//               test4(persons);
+//               test5(persons);
+//               test6(persons);
+               test7(persons);
+
+
+    }
+
+
+    private static void test1(List<Person> persons){
+
+        List<Person> filtered =
+                persons.stream()
+                .filter(p -> p.name.startsWith("P"))
+                .collect(Collectors.toList());
+        System.out.println(filtered);
+    }
+
+    private static void test2(List<Person> persons){
+        Map<Integer,List<Person>> personsByAge = persons
+                .stream()
+                .collect(Collectors.groupingBy(p ->p.age));
+
+         personsByAge.forEach((age,p)-> System.out.format("age %s:%s\n",age,p));
+
+    }
+
+    private static void test3(List<Person> persons){
+        Double averageAge = persons.stream()
+                .collect(Collectors.averagingInt(p -> p.age));
+
+        System.out.println(averageAge);
+    }
+
+    private static void test4(List<Person> persons){
+
+        IntSummaryStatistics ageSummary = persons.stream()
+                .collect(Collectors.summarizingInt(p->p.age));
+        System.out.println(ageSummary);
+    }
+
+
+    private static void test5(List<Person> persons){
+        String names = persons.stream().
+                filter(p->p.age >=18)
+                .map(p -> p.name)
+                .collect(Collectors.joining("and","In Germany","are of legal age."));
+        System.out.println(names);
+    }
+
+    private static void test6(List<Person> persons){
+        Map<Integer,String> map = persons
+                .stream()
+                .collect(Collectors.toMap(p->p.age,
+                        p->p.name,(name1,name2)->name1+";"+name2));
+        System.out.println(map);
+
+    }
+
+    private  static void test7(List<Person> persons){
+
+        Collector<Person,StringJoiner,String> personNameCollector =
+                Collector.of(
+                        () -> new StringJoiner("|"),
+                        (j,p) -> j.add(p.name.toUpperCase()),
+                        (j1,j2) -> j1.merge(j2),
+                        StringJoiner::toString
+                );
+                String names = persons.
+                        stream().
+                        collect(personNameCollector);
+           System.out.println(names);
+    }
+
+}
