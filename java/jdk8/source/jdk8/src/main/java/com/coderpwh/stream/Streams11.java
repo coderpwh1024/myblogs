5b83e4c6c27da6f83fbc27f522cd0885c9020153
coderpwh <pengwenhao@aliyun.com>
2018-01-03 11:42:57 +0800

    update java8 code
    update java8 code

--- java/jdk8/source/jdk8/src/main/java/com/coderpwh/stream/Streams11.java
+++ java/jdk8/source/jdk8/src/main/java/com/coderpwh/stream/Streams11.java
@@ -37,7 +37,9 @@ public class Streams11 {
 //        test1(list);
 //        test2(list);
 //        test3(list);
-        test4(list);
+//        test4(list);
+//        test5(list);
+           test6(list);
 
 
     }
@@ -78,6 +80,33 @@ public class Streams11 {
                  });
 
 
+    }
+
+
+    public  static  void test5(List<Person>persons){
+
+        Integer ageSum = persons.parallelStream().reduce(0,
+                (sum,p)->{
+               System.out.format("accumulator:sum=%s;person=%s\n",sum,p);
+                return  sum+=p.age;
+                },
+                (sum1,sum2)->{
+                System.out.format("combiner:sum1=%s;sum2=%s\n",sum1,sum2);
+                 return sum1+sum2;
+                });
+    }
+
+    private static void test6(List<Person> persons){
+
+        Integer ageSum = persons.parallelStream()
+                .reduce(0,(sum,p)->{
+                    System.out.format("accumulator: sum=%s;person=%s;thread=%s\n",sum,p,Thread.currentThread().getName());
+                    return sum+=p.age;
+                },
+                        (sum1,sum2)->{
+                    System.out.format("combiner:sum=%s;sum=%s;thread=%s\n",sum1,sum2,Thread.currentThread().getName());
+                    return sum1+sum2;
+                });
     }
 
 
