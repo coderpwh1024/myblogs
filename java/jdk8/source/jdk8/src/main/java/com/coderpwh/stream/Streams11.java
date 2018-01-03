fc60dde74d7a0b453ce2b6848c60273076d41dd3
coderpwh <pengwenhao@aliyun.com>
2018-01-03 08:56:13 +0800

    Update Streams11.java
    update java8 code

--- java/jdk8/source/jdk8/src/main/java/com/coderpwh/stream/Streams11.java
+++ java/jdk8/source/jdk8/src/main/java/com/coderpwh/stream/Streams11.java
@@ -34,14 +34,49 @@ public class Streams11 {
                 new Person("Peter", 23),
                 new Person("Pamela", 23),
                 new Person("David", 12));
-        test1(list);
-
+//        test1(list);
+//        test2(list);
+//        test3(list);
+        test4(list);
 
 
     }
 
     public static void test1(List<Person> persons){
         persons.stream().reduce((p1,p2)->p1.age>p2.age?p1:p2).ifPresent(System.out::println);
+
+    }
+
+    public static void test2(List<Person> persons){
+
+         Person result = persons.stream().reduce(new Person("",0),(p1,p2)->{
+             p1.age+=p2.age;
+             p1.name+=p2.name;
+             return p1;
+         });
+
+         System.out.format("name=%s;age=%s",result.name,result.age);
+    }
+
+    public static void test3(List<Person> persons){
+
+        Integer ageSum = persons.stream().reduce(0,(sum,p)->sum+=p.age,(sum1,sum2)->sum1+sum2);
+
+        System.out.println(ageSum);
+
+    }
+
+
+    public static void test4(List<Person> persons){
+         Integer ageSum = persons.stream().reduce(0,(sum,p)->{
+             System.out.format("accumulator:sum=%s;person=%s\n",sum,p);
+             return  sum +=p.age;
+         },
+                 (sum1,sum2)->{
+             System.out.format("combiner: sum1=%s;sum2=%s\n",sum1,sum2);
+               return sum1+sum2;
+                 });
+
 
     }
 
