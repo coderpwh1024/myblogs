dbbf98ba3c17972d58d11b66aaf3bdfa76547f19
coderpwh <pengwenhao@aliyun.com>
2018-01-14 22:08:29 +0800

    Update Executors3.java
    update java8 code

--- java/jdk8/source/jdk8/src/main/java/com/coderpwh/concurrent/Executors3.java
+++ java/jdk8/source/jdk8/src/main/java/com/coderpwh/concurrent/Executors3.java
@@ -1,5 +1,7 @@
- package com.coderpwh.concurrent;
+package com.coderpwh.concurrent;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.*;
 
 /**
@@ -8,50 +10,72 @@ import java.util.concurrent.*;
 public class Executors3 {
 
     public static void main(String[] args) throws InterruptedException {
-          test1();
-         test2();
-         test3();
+//        test1();
+//        test2();
+//        test3();
+        test4();
 
     }
 
 
+    private static void test1() throws InterruptedException {
 
-   private static void test1() throws InterruptedException {
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(2);
+        Runnable task = () -> System.out.println("Scheduling:" + System.nanoTime());
+        int delay = 3;
+        ScheduledFuture<?> future = scheduledExecutorService.schedule(task, delay, TimeUnit.SECONDS);
+        TimeUnit.MILLISECONDS.sleep(1337);
 
-       ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(2);
-       Runnable task = ()-> System.out.println("Scheduling:"+System.nanoTime());
-       int delay = 3;
-       ScheduledFuture<?> future = scheduledExecutorService.schedule(task,delay, TimeUnit.SECONDS);
-       TimeUnit.MILLISECONDS.sleep(1337);
-
-       long  remainingDelay = future.getDelay(TimeUnit.MILLISECONDS);
-       System.out.printf("Rmaining Delay: %sms\n",remainingDelay);
+        long remainingDelay = future.getDelay(TimeUnit.MILLISECONDS);
+        System.out.printf("Rmaining Delay: %sms\n", remainingDelay);
 
     }
 
-    private static void test2(){
-       ScheduledExecutorService scheduledExecutorService =   Executors.newScheduledThreadPool(1);
-        Runnable task = ()-> System.out.println("Scheduling:"+System.nanoTime());
+    private static void test2() {
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+        Runnable task = () -> System.out.println("Scheduling:" + System.nanoTime());
         int initiaDelay = 0;
-        int period =1;
-        scheduledExecutorService.scheduleAtFixedRate(task,initiaDelay,period,TimeUnit.SECONDS);
+        int period = 1;
+        scheduledExecutorService.scheduleAtFixedRate(task, initiaDelay, period, TimeUnit.SECONDS);
 
     }
 
-    private static void test3(){
+    private static void test3() {
 
-         ScheduledExecutorService scheduledExecutorService =   Executors.newScheduledThreadPool(1);
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
 
-         Runnable task = ()->{
-             try {
-                 TimeUnit.SECONDS.sleep(2);
-                 System.out.println("Scheduling:"+System.nanoTime());
-             } catch (InterruptedException e) {
-                 e.printStackTrace();
-                 System.err.println("task interrupdated");
-             }
-         };
-         scheduledExecutorService.scheduleWithFixedDelay(task,0,1,TimeUnit.SECONDS);
+        Runnable task = () -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+                System.out.println("Scheduling:" + System.nanoTime());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.err.println("task interrupdated");
+            }
+        };
+        scheduledExecutorService.scheduleWithFixedDelay(task, 0, 1, TimeUnit.SECONDS);
+
+    }
+
+    private static void test4() throws InterruptedException {
+        ExecutorService executorService = Executors.newWorkStealingPool();
+
+        List<Callable<String>> callables = Arrays.asList(
+                () -> "task1",
+                () -> "task2",
+                () -> "task3",
+                () -> "task4");
+
+        executorService.invokeAll(callables)
+                .stream()
+                .map(future -> {
+                    try {
+                        return future.get();
+                    } catch (Exception e) {
+                        throw new IllegalStateException(e);
+                    }
+                }).forEach(System.out::println);
+        executorService.shutdown();
 
     }
 
