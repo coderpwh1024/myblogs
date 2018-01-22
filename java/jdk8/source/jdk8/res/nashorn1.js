d08b10b7fd5c50bef68f6171f209692fec19c281
coderpwh <pengwenhao@aliyun.com>
2018-01-22 14:55:48 +0800

    Add files via upload
    update java8 code

---
+++ java/jdk8/source/jdk8/res/nashorn1.js
@@ -0,0 +1,15 @@
+/**
+ * Created by coderpwh on 2018/1/22.
+ */
+
+
+var fun1 = function (name) {
+    print("Hi there from JavaScript,"+name);
+
+    return "greetings from JavaScript";
+
+}
+
+ var fun2 = function (object) {
+     print("JS Class Definition: "+Object.prototype.toString.call(object));
+ };
