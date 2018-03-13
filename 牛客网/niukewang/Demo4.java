9fd21985245f9045ff96ed09a98d553b22f77827
coderpwh <pengwenhao@aliyun.com>
2018-03-13 14:07:49 +0800

    Add files via upload
    update java code

--- 牛客网/niukewang/Demo4.java
+++ 牛客网/niukewang/Demo4.java
@@ -6,6 +6,71 @@ package com.coderpwh.niukewang;
  * @Description:
  */
 
+import org.testng.annotations.Test;
+
+/**
+
+ */
 public class Demo4 {
+
+    public int[] multiply(int[] A) {
+
+        int[] B = new int[A.length];
+        for (int i = 0; i < B.length; i++) {
+            int sum = 1;
+            for (int j = 0; j < A.length; j++) {
+                 int temp=1;
+                if (i == j) {
+                    temp = A[j];
+                    System.out.println("A[j]:"+A[j]);
+                    A[j]=1;
+                }
+                sum = sum * A[j];
+            }
+            B[i] = sum;
+            System.out.println("sum:" + B[i]);
+        }
+        return B;
+    }
+
+
+
+
+    public int  multiplyB(int[] A){
+
+        int B[] = new int[A.length];
+
+         int sum1 = 1;
+
+         int sum2 = 1;
+
+          // B[i]=sum1的前面部分
+           for(int i=0;i<B.length;i++){
+                for(int j=0;j<A.length;j++){
+                    if(i==0){
+                        sum1=1;
+                    }else{
+                        sum1=sum1*A[j-1];
+                    }
+                }
+           }
+
+        return  0;
+    }
+
+
+
+
+    @Test
+    public void test() {
+        int[] b = {1, 2, 3, 4, 5};
+        System.out.println(multiplyB(b));
+
+//        int[] c = multiplyB(b);
+       /* for (int i = 0; i < c.length; i++) {
+            System.out.print(c[i] + " ");
+        }*/
+
+    }
 
 }
