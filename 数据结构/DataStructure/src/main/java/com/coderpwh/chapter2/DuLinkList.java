19586c715356d3ab228b1d570fc98fabab5666e4
coder-PengWenHao <pengwenhao@aliyun.com>
2018-09-20 23:12:32 +0800

    update DataStructure

---
+++ 数据结构/DataStructure/src/main/java/com/coderpwh/chapter2/DuLinkList.java
@@ -0,0 +1,37 @@
+package com.coderpwh.chapter2;
+
+public class DuLinkList implements  Ilist {
+
+    
+    public void clear() {
+
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int length() {
+        return 0;
+    }
+
+    public Object get(int i) throws Exception {
+        return null;
+    }
+
+    public void insert(int i, Object x) throws Exception {
+
+    }
+
+    public void remove(int i) throws Exception {
+
+    }
+
+    public int indexOf(Object x) {
+        return 0;
+    }
+
+    public void display() {
+
+    }
+}
