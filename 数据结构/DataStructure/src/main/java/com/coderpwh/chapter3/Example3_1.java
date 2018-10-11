d16b139c7abaf2c04f2a9919a8f74f212f22d758
coder-PengWenHao <pengwenhao@aliyun.com>
2018-10-11 22:49:19 +0800

    update DataStructure

--- 数据结构/DataStructure/src/main/java/com/coderpwh/chapter3/Example3_1.java
+++ 数据结构/DataStructure/src/main/java/com/coderpwh/chapter3/Example3_1.java
@@ -1,5 +1,7 @@
 package com.coderpwh.chapter3;
 
+import java.util.Scanner;
+
 /**
  * 分隔符匹配问题
  */
@@ -44,6 +46,48 @@ public class Example3_1 {
     }
 
 
+    public boolean isLegal(String str) throws Exception {
+        if (!"".equals(str) && str != null) {
+            SqStack S = new SqStack(100);
+            int length = str.length();
+            for (int i = 0; i < length; i++) {
+                char c = str.charAt(i);
+                String t = String.valueOf(c);
+                if (i != length) {
+                    if (('/' == c && '*' == str.charAt(i + 1)) || ('*' == c && '/' == str.charAt(i + 1))) {
+                        t = t.concat(String.valueOf(str.charAt(i + 1)));
+                        ++i;
+                    }
+                }
+                // 左分隔符
+                if (LEFT == verifyFlag(t)) {
+                    S.push(t);
+                } else if (RIGHT == verifyFlag(t)) {
+                    if (S.isEmpty() || !matches(S.pop().toString(), t)) {
+                        throw new Exception("错误:Java语句不合法!");
+                    }
+                }
+            }
+            if (!S.isEmpty()) {
+                throw new Exception("错误:java语句不合法!");
+            }
+            return true;
+        } else {
+            throw new Exception("错误:java语句不合法!");
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        Example3_1 e = new Example3_1();
+        System.out.println("请输入分java语句:");
+        Scanner sc = new Scanner(System.in);
+        if(e.isLegal(sc.nextLine())){
+            System.out.println("Java语句合法!");
+        }else{
+            System.out.println("错误:Java语句不合法!");
+        }
+    }
 
 
 }
