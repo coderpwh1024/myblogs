e6d9add3960718f109b76dbd44aca88e3812774e
coderpwh <pengwenhao@aliyun.com>
2018-10-17 10:23:30 +0800

    Update CircleSqQueue.java
    update CireSqQueue

--- 数据结构/DataStructure/src/main/java/com/coderpwh/chapter3/CircleSqQueue.java
+++ 数据结构/DataStructure/src/main/java/com/coderpwh/chapter3/CircleSqQueue.java
@@ -1,6 +1,8 @@
-package com.coderpwh.chapter3;
+ package com.coderpwh.chapter3;
 
 /**
+ * 顺序 队列
+ *
  * @author coderpwh
  * @version V1.0
  * @date 2018/10/16 16:06
@@ -32,23 +34,90 @@ public class CircleSqQueue implements IQueue {
         return front == rear;
     }
 
+    // 队列的长度
     public int length() {
         return (rear - front + queueElem.length) % queueElem.length;
     }
 
+
+    /**
+     * 获取队列的头元素
+     *
+     * @return
+     */
     public Object peek() {
-        return null;
+        if (front == rear) {
+            return null;
+        } else {
+            return queueElem[front];
+        }
     }
 
+    /**
+     * 出队
+     *
+     * @return
+     */
     public Object poll() {
-        return null;
+        if (front == rear) {
+            return null;
+        } else {
+            Object t = queueElem[front];
+            front = (front + 1) % queueElem.length;
+            return t;
+        }
     }
 
+    /**
+     * 入队
+     *
+     * @param o
+     * @throws Exception
+     */
     public void offer(Object o) throws Exception {
+        if ((rear + 1) % queueElem.length == front) {
+            throw new Exception("队列已满");
+        } else {
+            queueElem[rear] = o;
+            rear = (rear + 1) % queueElem.length;
+        }
 
     }
 
     public void display() {
-
+        if (!isEmpty()) {
+            for (int i = front; i != rear; i = (i + 1) % queueElem.length) {
+                System.out.println(queueElem[i].toString());
+            }
+        } else {
+            System.out.println("此队列为空!");
+        }
     }
+
+
+    public Object[] getQueueElem() {
+        return queueElem;
+    }
+
+    public void setQueueElem(Object[] queueElem) {
+        this.queueElem = queueElem;
+    }
+
+    public int getFront() {
+        return front;
+    }
+
+    public void setFront(int front) {
+        this.front = front;
+    }
+
+    public int getRear() {
+        return rear;
+    }
+
+    public void setRear(int rear) {
+        this.rear = rear;
+    }
+
+
 }
