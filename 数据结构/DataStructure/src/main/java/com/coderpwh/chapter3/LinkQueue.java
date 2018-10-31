eb7c315438355163b71bcc8a1296aa9f74a21e91
coder-PengWenHao <pengwenhao@aliyun.com>
2018-10-31 22:42:33 +0800

    update DataStructure

--- 数据结构/DataStructure/src/main/java/com/coderpwh/chapter3/LinkQueue.java
+++ 数据结构/DataStructure/src/main/java/com/coderpwh/chapter3/LinkQueue.java
@@ -60,20 +60,51 @@ public class LinkQueue implements IQueue {
      * @return
      */
     public Object poll() {
-        return null;
+        if (front != null) {
+            Node p = front;
+            front = front.next;
+            if (p == rear) {
+                rear = null;
+            }
+            return p.data;
+        } else {
+            return null;
+        }
     }
 
     /**
      * 入队操作
+     * 不带头单链表插入，可以在表中或表尾插入
+     * 而链队只能在表中插入
      *
      * @param o
      * @throws Exception
      */
     public void offer(Object o) throws Exception {
-
+        Node p = new Node(o);
+        if (front != null) {
+            rear.next = p;
+            rear = p;
+        } else {
+            front = rear = p;
+        }
     }
 
+    /**
+     * 遍历链队
+     *
+     * @throws Exception
+     */
     public void display() throws Exception {
+        if (!isEmpty()) {
+            Node p = front;
+            while (p != null) {
+                System.out.println(p.data);
+                p = p.next;
+            }
+        } else {
+            System.out.println("此队列为空!");
+        }
 
     }
 
