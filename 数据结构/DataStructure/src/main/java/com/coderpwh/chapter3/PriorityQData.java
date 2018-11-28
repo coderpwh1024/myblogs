552b601b942c662e75e0f984baa61d42ac915204
coder-PengWenHao <pengwenhao@aliyun.com>
2018-11-28 23:06:22 +0800

    update DataStructure

---
+++ 数据结构/DataStructure/src/main/java/com/coderpwh/chapter3/PriorityQData.java
@@ -0,0 +1,15 @@
+package com.coderpwh.chapter3;
+
+/**
+ * 优先级队列结点
+ */
+public class PriorityQData {
+
+    public Object elem;
+    public int priority;
+
+    public PriorityQData(Object elem, int priority) {
+        this.elem = elem;
+        this.priority = priority;
+    }
+}
