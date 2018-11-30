9a27e7039cf9bd578fe67502cc35afb13fb1865c
coder-PengWenHao <pengwenhao@aliyun.com>
2018-11-30 23:44:09 +0800

    update DataStructure

--- 数据结构/DataStructure/src/main/java/com/coderpwh/chapter3/PriorityQueue.java
+++ 数据结构/DataStructure/src/main/java/com/coderpwh/chapter3/PriorityQueue.java
@@ -62,19 +62,56 @@ public class PriorityQueue implements IQueue {
      * @return
      */
     public Object poll() {
-        if(front==null){
-            return  null;
-        }else{
+        if (front == null) {
+            return null;
+        } else {
             Node p = front;
             front = p.next;
-            return  p.data;
+            return p.data;
         }
     }
 
-    public void offer(Object o) throws Exception {
+    /**
+     * 入队
+     *
+     * @param x
+     * @throws Exception
+     */
+    public void offer(Object x) throws Exception {
+        PriorityQData pn = (PriorityQData) x;
+        // 新节点
+        Node s = new Node(pn);
+        // 空队列
+        if (front == null) {
+            front = rear = s;
+        } else {
+            Node p = front, q = front;
+            // 新结点数据域值与队列结点的数据域相比较
+            while (p != null && pn.priority >= ((PriorityQData) p.data).priority) {
+                q = p;
+                p = p.next;
+            }
+            if (p == null) {   // 插入队尾
+                rear.next = s;
+                rear = s;
+            } else if (p == front) {  // 插入队首
+                s.next = front;
+                front = s;
+            } else {           // 插入 队中
+                q.next = s;
+                s.next = p;
+            }
+
+        }
+
 
     }
 
+    /**
+     * 循环遍历
+     *
+     * @throws Exception
+     */
     public void display() throws Exception {
 
     }
