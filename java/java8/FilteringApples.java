10af26bc55ba45618ef8b440da4675a4e13db100
coderpwh <pengwenhao@aliyun.com>
2019-03-14 19:57:45 +0800

    add java8
    add java8

---
+++ java/java8/FilteringApples.java
@@ -0,0 +1,12 @@
+package com.coderpwh.code.package5;
+
+/**
+ * 2 * @Author: pengwenhao
+ * 3 * @Date: 2019/3/14 19:56
+ * 4
+ */
+public class FilteringApples {
+    public static void main(String[] args) {
+
+    }
+}
