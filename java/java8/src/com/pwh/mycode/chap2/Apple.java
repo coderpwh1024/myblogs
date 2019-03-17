0e616a7ce3b18ee393bd6ca213f67eefc6c50a95
coder-PengWenHao <pengwenhao@aliyun.com>
2019-03-17 17:25:26 +0800

    update java8 code

---
+++ java/java8/src/com/pwh/mycode/chap2/Apple.java
@@ -0,0 +1,42 @@
+package com.pwh.mycode.chap2;
+
+/**
+ *   实体类 Apple
+ * @author coderpwh
+ * @create 2019-03-17 17:21
+ * @desc ${DESCRIPTION}
+ **/
+public   class Apple {
+
+    private int weight = 0;
+    private String color = "";
+
+    public Apple(int weight, String color) {
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String toString() {
+        return "Apple{" +
+                "color='" + color + '\'' +
+                ", weight=" + weight +
+                '}';
+
+    }
+}
---
+++ java/java8/src/com/pwh/mycode/chap2/ApplePredicate.java
@@ -0,0 +1,12 @@
+package com.pwh.mycode.chap2;
+
+/**
+ *
+ * @author coderpwh
+ * @create 2019-03-17 17:23
+ * @desc ${DESCRIPTION}
+ **/
+public interface ApplePredicate {
+
+    public boolean test(Apple a);
+}
