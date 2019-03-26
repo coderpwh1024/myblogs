1efdcd79dce9bfd8b03db16b246fcc2011b3ac5d
coderpwh <pengwenhao@aliyun.com>
2019-03-26 19:32:09 +0800

    Add files via upload

---
+++ java/java8/src/com/pwh/mycode/chap2/AppleColorPredicate.java
@@ -0,0 +1,16 @@
+package com.pwh.mycode.chap2;
+
+/**
+ * 颜色比较
+ * 2 * @Author: pengwenhao
+ * 3 * @Date: 2019/3/26 15:52
+ * 4
+ */
+public class AppleColorPredicate implements ApplePredicate {
+
+
+    @Override
+    public boolean test(Apple apple) {
+        return "green".equals(apple.getColor());
+    }
+}
