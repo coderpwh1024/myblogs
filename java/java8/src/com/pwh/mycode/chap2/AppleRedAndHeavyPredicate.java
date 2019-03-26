c7e0bb9d825fbae3cef3043b3570574a3590a36a
coderpwh <pengwenhao@aliyun.com>
2019-03-26 19:31:16 +0800

    Add files via upload

---
+++ java/java8/src/com/pwh/mycode/chap2/AppleRedAndHeavyPredicate.java
@@ -0,0 +1,15 @@
+package com.pwh.mycode.chap2;
+
+/**
+ * 2 * @Author: pengwenhao
+ * 3 * @Date: 2019/3/26 15:53
+ * 4
+ */
+public class AppleRedAndHeavyPredicate implements ApplePredicate {
+
+
+    @Override
+    public boolean test(Apple apple) {
+        return "red".equals(apple.getColor()) && apple.getWeight() > 150;
+    }
+}
