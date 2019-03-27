6034bafb21b6bd6e525b5c7071f6212e1175207d
coder-PengWenHao <pengwenhao@aliyun.com>
2019-03-27 22:29:10 +0800

    update java8

---
+++ java/java8/src/com/pwh/mycode/chap3/ApplePredicate.java
@@ -0,0 +1,11 @@
+package com.pwh.mycode.chap3;
+
+/**
+ * @author coderpwh
+ * @create 2019-03-27 22:27
+ * @desc ${DESCRIPTION}
+ **/
+public interface ApplePredicate {
+    
+    public boolean test(Apple apple);
+}
