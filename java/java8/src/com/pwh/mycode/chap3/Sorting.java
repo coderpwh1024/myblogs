8827e56f31e1e9d19b307db4d19c9ec9faefae8c
coder-PengWenHao <pengwenhao@aliyun.com>
2019-04-07 16:24:00 +0800

    update java8 code

--- java/java8/src/com/pwh/mycode/chap3/Sorting.java
+++ java/java8/src/com/pwh/mycode/chap3/Sorting.java
@@ -1,11 +1,50 @@
 package com.pwh.mycode.chap3;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.util.Comparator.comparing;
+
 /**
  * @author coderpwh
  * @create 2019-04-07 16:05
  * @desc ${DESCRIPTION}
  **/
 public class Sorting {
+    public static void main(String[] args) {
+        List<Apple> inventory = new ArrayList<>();
+        inventory.addAll(Arrays.asList(new Apple(80, "green"), new Apple(155, "green"), new Apple(120, "red")));
+
+        inventory.sort(new AppleComparator());
+        System.out.println(inventory);
+
+        inventory.set(1, new Apple(30, "green"));
+
+
+        inventory.sort(new Comparator<Apple>() {
+            public int compare(Apple a1, Apple a2) {
+                return a1.getWeight().compareTo(a2.getWeight());
+            }
+        });
+        System.out.println(inventory);
+
+        inventory.set(1, new Apple(20, "red"));
+
+
+        inventory.sort((a1, a2) -> a1.getWeight().compareTo(a2.getWeight()));
+        System.out.println(inventory);
+
+
+        inventory.set(1, new Apple(10, "red"));
+
+
+        inventory.sort(comparing(Apple::getWeight));
+        System.out.println(inventory);
+
+
+    }
 
 
 }
