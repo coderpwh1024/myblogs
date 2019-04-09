06b94fd7c5dcab760a9399a1b4e88042f676cb6c
coder-PengWenHao <pengwenhao@aliyun.com>
2019-04-09 23:10:51 +0800

    update java8

--- java/java8/src/com/pwh/mycode/chap4/StreamVsCollection.java
+++ java/java8/src/com/pwh/mycode/chap4/StreamVsCollection.java
@@ -1,5 +1,9 @@
 package com.pwh.mycode.chap4;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 /**
  * @author coderpwh
  * @create 2019-04-09 23:06
@@ -7,6 +11,8 @@ package com.pwh.mycode.chap4;
  **/
 public class StreamVsCollection {
     public static void main(String[] args) {
-
+        List<String> names = Arrays.asList("Java8", "Lambdas", "In", "Action");
+        Stream<String> s = names.stream();
+        s.forEach(System.out::println);
     }
 }
