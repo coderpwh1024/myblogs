d61c7a0c4bff82598ae4ab1b3871d1ac4c68d037
coderpwh <pengwenhao@aliyun.com>
2019-04-19 17:11:04 +0800

    Add files via upload
    add Grouping

---
+++ java/java8/src/com/pwh/mycode/chap6/Grouping.java
@@ -0,0 +1,38 @@
+package com.pwh.mycode.chap6;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static com.pwh.mycode.chap6.Dish.dishTags;
+import static com.pwh.mycode.chap6.Dish.menu;
+import static java.util.stream.Collectors.*;
+
+/**
+ * 2 * @Author: pengwenhao
+ * 3 * @Date: 2019/4/19 14:52
+ * 4
+ */
+public class Grouping {
+
+    public static void main(String[] args) {
+
+        System.out.println("Dishes grouped by type: " + groupDishesByType());
+        System.out.println("Dish names grouped by type: " + groupDishNamesByType());
+    }
+
+
+    private static Map<Dish.Type, List<Dish>> groupDishesByType() {
+        return menu.stream().collect(groupingBy(Dish::getType));
+    }
+
+    private static  Map<Dish.Type,List<Integer>> groupDishNamesByType(){
+        return  menu.stream().collect(groupingBy(Dish::getType,mapping(Dish::getCalories,toList())));
+    }
+
+
+
+
+
+
+}
