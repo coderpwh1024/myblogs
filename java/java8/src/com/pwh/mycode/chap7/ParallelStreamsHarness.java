a26fbb3dfac62dd99795befbb41f5316de443333
coderpwh <pengwenhao@aliyun.com>
2019-04-23 17:10:56 +0800

    Add files via upload
    add ParallerStreamsHarness

---
+++ java/java8/src/com/pwh/mycode/chap7/ParallelStreamsHarness.java
@@ -0,0 +1,65 @@
+package com.pwh.mycode.chap7;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.function.Function;
+
+/**
+ * 2 * @Author: pengwenhao
+ * 3 * @Date: 2019/4/23 15:24
+ * 4
+ */
+public class ParallelStreamsHarness {
+
+
+    public static final ForkJoinPool FORK_JOIN_POOL = new ForkJoinPool();
+
+    public static void main(String[] args) {
+    /*    System.out.println("Sequential sum done in:" +
+                measureSumPerf(ParallelStreams::sequentialSum, 10_000_000) + " msecs");*/
+
+  /*      System.out.println("Sequential sum done in:" +
+                measureSumPerf(ParallelStreams::iterativeSum, 10_000_000) + " msecs");*/
+
+/*        System.out.println("Sequential sum done in:" +
+                measureSumPerfs(ParallelStreams::parallelSum, 10_000_000) + " msecs");*/
+
+     /*   System.out.println("Iterative Sum done in: " + measurePerf(ParallelStreams::iterativeSum, 10_000_000L) + " msecs");
+        System.out.println("Sequential Sum done in: " + measurePerf(ParallelStreams::sequentialSum, 10_000_000L) + " msecs");
+        System.out.println("Parallel forkJoinSum done in: " + measurePerf(ParallelStreams::parallelSum, 10_000_000L) + " msecs");
+        System.out.println("Range forkJoinSum done in: " + measurePerf(ParallelStreams::rangedSum, 10_000_000L) + " msecs");
+        System.out.println("Parallel range forkJoinSum done in: " + measurePerf(ParallelStreams::parallelRangedSum, 10_000_000L) + " msecs");*/
+
+//        System.out.println("SideEffect sum done in: " + measurePerf(ParallelStreams::sideEffectSum, 10_000_000L) + " msecs");
+        System.out.println("SideEffect prallel sum done in: " + measurePerf(ParallelStreams::sideEffectParallelSum, 10_000_000L) + " msecs");
+    }
+
+
+    public static <T, R> long measurePerf(Function<T, R> f, T input) {
+        long fastest = Long.MAX_VALUE;
+        for (int i = 0; i < 10; i++) {
+            long start = System.nanoTime();
+            R result = f.apply(input);
+            long duration = (System.nanoTime() - start) / 1_000_000;
+            System.out.println("Result: " + result);
+            if (duration < fastest) fastest = duration;
+        }
+        return fastest;
+    }
+
+
+    public static long measureSumPerfs(Function<Long, Long> adder, long n) {
+        long fastest = Long.MAX_VALUE;
+        for (int i = 0; i < 10; i++) {
+            long start = System.nanoTime();
+            long sum = adder.apply(n);
+            long duration = (System.nanoTime() - start) / 1_000_100;
+            System.out.println("Result:" + sum);
+            if (duration < fastest) {
+                fastest = duration;
+            }
+        }
+        return fastest;
+    }
+
+
+}
