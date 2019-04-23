d8ae395337e03d43c95c8ebd6c3297e899d904f2
coderpwh <pengwenhao@aliyun.com>
2019-04-23 15:58:30 +0800

    Add files via upload
    add  Accumulator

---
+++ java/java8/src/com/pwh/mycode/chap7/Accumulator.java
@@ -0,0 +1,16 @@
+package com.pwh.mycode.chap7;
+
+/**
+ * 2 * @Author: pengwenhao
+ * 3 * @Date: 2019/4/23 15:17
+ * 4
+ */
+public class Accumulator {
+
+    public long total = 0;
+
+    public void add(long value) {
+        total += value;
+    }
+
+}
