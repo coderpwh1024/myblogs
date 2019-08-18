a584afd4d7a74939a156aa35fdbb768cb8d20243
coder-PengWenHao <pengwenhao@aliyun.com>
2019-08-18 16:46:47 +0800

    update core-patterns

---
+++ core-patterns/src/com/coderpwh/code/creation/singletion/v2_2/DoubleCheckLockingSingleton.java
@@ -0,0 +1,28 @@
+package com.coderpwh.code.creation.singletion.v2_2;
+
+/**
+ * @author coderpwh
+ * @create 2019-08-18 15:08
+ * @desc ${DESCRIPTION}
+ **/
+public class DoubleCheckLockingSingleton {
+
+    private static volatile DoubleCheckLockingSingleton INSTANCE;
+
+    private DoubleCheckLockingSingleton() {
+
+    }
+
+    public static DoubleCheckLockingSingleton getINSTANCE() {
+        if (INSTANCE == null) {
+            synchronized (DoubleCheckLockingSingleton.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new DoubleCheckLockingSingleton();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+
+}
