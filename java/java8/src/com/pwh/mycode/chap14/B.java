579bd9ec3b7159373aaa425031670e4a84ef77ab
coder-PengWenHao <pengwenhao@aliyun.com>
2020-03-24 13:47:46 +0800

    update java code

--- java/java8/src/com/pwh/mycode/chap14/B.java
+++ java/java8/src/com/pwh/mycode/chap14/B.java
@@ -26,7 +26,7 @@ public class B extends  A {
     }
 
 
-    
+
 
 
 
---
+++ java/java8/src/com/pwh/mycode/chap14/Box.java
@@ -0,0 +1,14 @@
+package com.pwh.mycode.chap14;
+
+/**
+ * @author coderpwh
+ * @create 2020-03-23 11:05
+ * @desc ${DESCRIPTION}
+ **/
+public class Box {
+    public static void main(String[] args) {
+        A ab = new B();
+        System.out.println("--------------------------");
+        ab = new  B();
+    }
+}
