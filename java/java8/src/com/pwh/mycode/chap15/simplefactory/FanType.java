f1cce14914a8971bfc1f307f8aa49bbde6977647
coder-PengWenHao <pengwenhao@aliyun.com>
2020-03-25 10:56:36 +0800

    update java code

---
+++ java/java8/src/com/pwh/mycode/chap15/simplefactory/FanType.java
@@ -0,0 +1,16 @@
+package com.pwh.mycode.chap15.simplefactory;
+
+/**
+ * @author coderpwh
+ * @create 2020-03-25 10:50
+ * @desc ${DESCRIPTION}
+ **/
+public enum  FanType {
+
+     TableFan,  // 台扇
+
+    CeilingFan,  // 吊扇
+
+    ExhaustFan // 排风扇
+
+}
---
+++ java/java8/src/com/pwh/mycode/chap15/simplefactory/IFan.java
@@ -0,0 +1,14 @@
+package com.pwh.mycode.chap15.simplefactory;
+
+/**
+ * @author coderpwh
+ * @create 2020-03-25 10:49
+ * @desc ${DESCRIPTION}
+ **/
+public interface IFan {
+
+    public void  swithOn();
+
+    public void  witchOff();
+
+}
