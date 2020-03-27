dfebe85700d29fa189eb919d08f894f842706c52
coder-PengWenHao <pengwenhao@aliyun.com>
2020-03-27 14:14:06 +0800

    update java code

---
+++ java/java8/src/com/pwh/mycode/chap15/proxy/IFolder.java
@@ -0,0 +1,12 @@
+package com.pwh.mycode.chap15.proxy;
+
+/**
+ * @author coderpwh
+ * @create 2020-03-27 14:12
+ * @desc ${DESCRIPTION}
+ **/
+public interface IFolder {
+
+     public void performOperations();
+
+}
